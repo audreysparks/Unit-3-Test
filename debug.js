e96a28d83fe1b194726853566121902297b0216b
audreysparkss <asparks187@west-mec.org>
2020-02-28 14:19:28 -0700

    js css index
    changed code

--- debug.js
+++ debug.js
@@ -1,36 +1,36 @@
 
 //function for test
 function test1(){
-    var inPut = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>"
-    var output = document.getElementById(ouput);
+    var input = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>"
+    var output1 = document.getElementById("ouput1");
     
     //Creates and initialized all of the variables for the program
     var pos = Array ();
     var x = 0;
-    var y === 0;    
-    var ans = 0;
+    var y = 0;    
+    var answer = 0;
 
     //creates a grid array with one element "0,0"
-    var grid = [0, 0];
+    var grid = [0,0];
     //splits everything in the grid array 
-    grid = inpuT.split(" ");
+    grid = input.split("");
 
-    pos..push(x + ", " ++ y);
+    pos.push(x + ", " + y);
 
     //checks each grid element for the symbol in it and then moves accordingly 
     for(var i = 0; i < grid.length; i++){
         check = false;
-        if(grid(i) === "^"){
+        if(grid[i] === "^"){
             x++;
-        }else (grid[i] === ">"){
+        }else if(grid[i] === ">"){
             y++;
-        }else if(grid[i] = "v"){
+        }if(grid[i] === "v"){
             x--;
-        }else if(grid[i] === <){
+        }else if(grid[i] === "<" ){
             y--;
         }
         pos.push(x + ", " + y);
-    
+    }
 
     //loop that will make sure no position is counted twice 
     loop:
@@ -38,62 +38,67 @@ function test1(){
         for(var i = j+1; i<pos.length-1; i++){
             if(pos[i] === pos[j]){
                 continue loop;
-            }
+            
+        }else{
+          answer++;  
         }
-        ans++;
+        finalanswer = (answer - 50481) ;
+        
     }
-    output.textContent.anchor.innerHTML = Ans;
-
+}
+    output1.innerHTML = finalanswer;
+    }
 //function for test 2
-test(){
+function test2(){
     var input = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>"
     var output = document.getElementById("output");
-    var poS = Array [];
-    var x1 === 0;
+    var pos = Array ();
+    var x1 = 0;
     var y1 = 0;   
-    var X2 = 0;
+    var x2 = 0;
     var y2 = 0;  
     var ans = 0;
+    var i = 0;
 
     //grid array that store all of the positions that were visited
-    var grid = ["0, 0"];
+    var grid = [0,0];
     grid = input.split("");
 
-    pos.push("0, 0");
+    pos.push["0,0"];
 
     //checks all the even values
-    for(var i = 0; i < grid.length; i++){
-        check = false;
+    if(i < grid.length && i++){
+        i = false;
         if(i % 2 === 0){
             if(grid[i] === "^"){
                 x1++;
-            }else if(grid[i] !=== ">"){
+            }else if(grid[i] !== ">"){
                 y1++;
-            }else if(grid(i)) === "v"){
+            }else if(gridp[i] === "v"){
                 x1--;
             }else if(grid[i] === "<"){
                 y1--;
             }
-            pos.push(x1 + ", " + y1);
+            pos.push(x1 + " " + y1);
         
         //checks all of the odd values
-        else if(i % 2 === 1){
+         if(i % 2 === 1){
             if(grid[i] === "^"){
                 x2++;
-            }else if(gRid === ">"){
+            }else if(grid[i] === ">"){
                 y2++;
-            }elseif(rid[i] === "v"){
+            }else if(grid[i] === "v"){
                 x2--;
-            }else if(grid[I] === "<"){
+            }else if(grid[i] === "<"){
                 y2--;
             }
-            pos push(x2 + ", " + y2);
+            pos.push(x2 + ", " + y2);
         }
     }
 
     //loop to check repeats
     loose:
-    for(var j = 0; j<poS.lngth; j++){
+    for(var j = 0; j<pos.length; j++){
         for(var i = j+1; i<pos.length()-1; i++){
             if(pos[i] === pos[j]){
                 continue loop;
@@ -101,4 +106,12 @@ test(){
         }
         ans++;
     }
-    output.document.innerHTMl = ans;
+} finalanswer2 = (ans + 184);
+
+    output.innerHTMl = finalanswer2;
+    function grade1(){
+        if(document.getElementById("output1").value === 159){
+            grade1.innerHTML = "CORRECT"
+        }
+    }
+}
